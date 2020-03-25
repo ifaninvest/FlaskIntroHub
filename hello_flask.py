a599425f91ba4eddd87854988cd999705b68b57e
ifaninvest <ifaninvest@gmail.com>
2020-03-25 17:02:59 -0400

    First Commit

--- hello_flask.py
+++ hello_flask.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hello from Branch V2'
+    return 'hello from Local Master Branch /n hello Git1'
+
 
 if __name__=='__main__':
     app.run(debug=True)
